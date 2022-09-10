948df48b47c357f5bc7f7c5b4896f1f2005e8a1f
Toky Ratolojanahary <tokyrt.pro@gmail.com>
2022-09-11 02:51:37 +0300

    animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 (function () {
-  //overlay loader
-  var overlay = document.querySelector("body>.overlay");
-
-  window.addEventListener("load", function () {
-    overlay.classList.add("loaded");
+  
+  const aiguille = document.getElementById('aiguille');
+  console.log(aiguille);
+  document.addEventListener('mousemove', function(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    const aX = aiguille.getBoundingClientRect().left + aiguille.getBoundingClientRect().width/2;
+    const aY = aiguille.getBoundingClientRect().top+ aiguille.getBoundingClientRect().height/2;
+    let deltaX = x - aX;
+    let deltaY = y - aY;
+    let cosTeta = deltaX/(Math.sqrt(deltaX * deltaX + deltaY * deltaY));
+    let angle = Math.acos(cosTeta) * 180 / Math.PI;
+    let sign = y < aY ? -1 : 1;
+    aiguille.style.transform = `rotate(${sign*angle}deg)`;
   });
-
   
 })();
